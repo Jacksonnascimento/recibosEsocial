a76bc2f06fe84b2cf42ab1eb01225db70a9dad85
Jackson Nascimento <44805133+Jacksonnascimento@users.noreply.github.com>
2025-05-28 20:30:03 -0300

    git

---
+++ src/main/java/com/recibos/recibos_service/RecibosServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recibos.recibos_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecibosServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecibosServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recibos/recibos_service/RecibosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recibos.recibos_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecibosServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
